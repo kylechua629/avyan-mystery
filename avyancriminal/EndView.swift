e9fd0a2b62238b04124721bfbaa708a918baaf2c
kyle <kyle.henric.chua@gmail.com>
2024-10-21 15:23:04 +0800

    finished the app

---
+++ avyancriminal/EndView.swift
@@ -0,0 +1,18 @@
+//
+//  EndView.swift
+//  avyancriminal
+//
+//  Created by Kyle Henric Buguina Chua on 21/10/24.
+//
+
+import SwiftUI
+
+struct EndView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EndView()
+}
